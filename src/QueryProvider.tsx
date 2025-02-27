56d136990064cda95826ac25df040308a4edf966
dmdgpdi <dmdgpdi@naver.com>
2025-02-28 00:57:22 +0900

    chore: tanstack query 의존성 설치 및 적용

---
+++ src/QueryProvider.tsx
@@ -0,0 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import type { PropsWithChildren } from "react";
+
+const queryClient = new QueryClient();
+
+export const QueryProvider = ({ children }: PropsWithChildren) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children} <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryProvider } from "./QueryProvider.tsx";
 import { RouteInfoProvider } from "./RouteInfoProvider.tsx";
 import { Routes } from "./Routes.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouteInfoProvider>
-      <Routes />
-    </RouteInfoProvider>
+    <QueryProvider>
+      <RouteInfoProvider>
+        <Routes />
+      </RouteInfoProvider>
+    </QueryProvider>
   </StrictMode>,
 );
